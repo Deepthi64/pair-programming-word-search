2b3886da719bd83a6d2e01b5f7985eef1c5b90a5
deepthi vuyyuru <vdeepthi.dara@gmail.com>
2023-04-13 00:19:02 -0400

    intial commit

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,29 @@
-const wordSearch = (letters, word) => { 
-    const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
-    }
-}
 
+const wordSearch = (letters, word) => {
+  if (letters.length) {
+    const horizontalJoin = letters.map(ls => ls.join(''));
+    for (let l of horizontalJoin) {
+      if (l.includes(word)) return true;
+    }
+    const verticalJoin = [];
+    let vertLet = 0;
+
+    for (let i = 0; i < letters[0].length; i++) {
+      let arr = [];
+      for (let letter of letters) {
+        arr.push(letter[vertLet]);
+      }
+      vertLet += 1;
+      verticalJoin.push(arr.join(''));
+    }
+
+    for (let j of verticalJoin) {
+      if (j.includes(word)) {
+        return true;
+      }
+    }
+
+    return false;
+  }
+};
 module.exports = wordSearch
